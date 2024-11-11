7b61e704545bb31504fec3c23ac0d1fd14780ac3
melvinsamuel070 <melvinsamuel070@gmail.com>
2024-11-11 10:26:31 +0100

    new

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -26,7 +26,7 @@ function TodoItem({ todo, index, moveTodo, setData }) {
 
     const deleteTodo = async (id) => {
         try {
-            let response = await axios.delete(`http://34.192.51.127:3500/todo/${id}`)
+            let response = await axios.delete(`http://localhost:3500/todo/${id}`)
             if(response.data.status){
                 setData(response.data.data)
             }
