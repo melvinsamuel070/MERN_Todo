fb834ef87e8b9ece02c91586fe60e6ddd19ce8d8
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-02-24 12:07:51 +0100

    new

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     transform: {
       "^.+\\.jsx?$": "babel-jest",
-      "^.+\\.css$": "jest-css-modules-transform",
+      "^.+\\.css$": "node-modules",
     },
     testEnvironment: "jsdom", // Required for testing React components
   };
