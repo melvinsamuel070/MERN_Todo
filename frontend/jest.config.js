d308ead604a3617bed4a17fc2cc381236b894e6e
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-02-25 18:00:39 +0100

    new

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -9,11 +9,20 @@
 // };
 
 module.exports = {
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"  // Ensures Jest processes ES modules correctly
+  },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js', // For CSS files
+    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
     "\\.(svg|jpg|jpeg|png|gif|css|scss)$": "<rootDir>/__mocks__/fileMock.js"
   },
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
+  extensionsToTreatAsEsm: [".jsx", ".js"],  // Treats JS files as ES modules
+  globals: {
+    "ts-jest": {
+      useESM: true
+    }
+  }
 };
 
 
