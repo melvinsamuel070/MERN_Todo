82e9283e93366f6f83263e79757a991920ee9063
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-02-25 13:19:17 +0100

    new

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,17 @@
+// module.exports = {
+//   transform: {
+//     "^.+\\.jsx?$": "babel-jest",
+//   },
+//   moduleNameMapper: {
+//     "\\.(css|less|scss|sass)$": "jest-transform-stub",
+//   },
+//   testEnvironment: "jest-environment-jsdom",  // This ensures Jest uses JSDOM
+// };
+
+// jest.config.js
 module.exports = {
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
+  // ... other config options ...
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "jest-transform-stub",
+    '\\.(css)$': 'identity-obj-proxy'
   },
-  testEnvironment: "jest-environment-jsdom",  // This ensures Jest uses JSDOM
 };
